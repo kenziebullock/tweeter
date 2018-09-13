d6cae8833c0bfabaace4d53c5bd2b59ff9d447c2
Kenzie Bullock <kenzieb_13@shaw.ca>
2018-09-13 17:02:27 +0000

    added hide/show compose tweet button and error messages

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,54 +4,31 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 // Fake data taken from tweets.json
-const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
-  
-function createTweetElement(tweetObject) {
+
+// code refactor starts here!
+// $(`<header class="tweet-header">
+//                         <img class="tweet-avatar" src=${tweetObject.user.avatars.small}>
+//                         <h4 class= "tweet-name">${tweetObject.user.name}</h4>
+//                         <h6 class="tweet-handle">${tweetObject.user.handle}</h6>
+//                     </header>`);
+
+
+// const tweetHeader = function(headerInput) {
+//     let $header = $('<header>', {'class': 'tweet-header'})
+//         .append($('<img>', {'class': 'tweet-avatar', 'src': headerInput.avatars.small}))
+//         .append($('<h4>', {'class': 'tweet-name'}).text(headerInput.name))
+//         .append('<h6>', {'class': 'tweet-handle'}).text(headerInput.handle);
+//     console.log($header);
+//     return $header;
+// }
+
+// const tweetBody = function(bodyInput) {
+
+// }
+
+
+
+const createTweetElement = function(tweetObject) {
     
     let $newTweet = $("<article>").addClass("tweet-container");
     let header = $(`<header class="tweet-header">
@@ -59,10 +36,11 @@ function createTweetElement(tweetObject) {
                         <h4 class= "tweet-name">${tweetObject.user.name}</h4>
                         <h6 class="tweet-handle">${tweetObject.user.handle}</h6>
                     </header>`);
-    let body = $(`<p class="tweet-body">${tweetObject.content.text}</p>`);
+    let body = $('<p>', {'class': 'tweet-body'}).text(tweetObject.content.text);
+    // $(`<p class="tweet-body">${tweetObject.content.text}</p>`);
     let footer = $(`<footer class="tweet-footer"> 
                         <div class ="footer-items">
-                            <p class="date">${tweetObject.created_at}</p>
+                            <p class="date">${Date(tweetObject.created_at)}</p>
                         </div>
                         <div class="footer-items">
                             <img class="glyphs" src="/images/flag.png">
@@ -77,10 +55,56 @@ function createTweetElement(tweetObject) {
 function renderTweets(tweets) {
     tweets.forEach(function(tweet) {
         let $currentTweet = createTweetElement(tweet);
-        $('.container').append($currentTweet);
+        $('.new-tweets').prepend($currentTweet);
     });
 }
 
 $( document ).ready(function() {
-    renderTweets(data);
+    function loadTweets() {
+        $.ajax('/tweets', { method: 'GET' })
+        .then(function(tweets) {
+            renderTweets(tweets);
+        });
+    }
+    loadTweets();
+    
+    $('#submitTweet').on('click', function(event) {
+        event.preventDefault();
+        
+        // grab content of tweet
+        let tweetData = $('#tweet-text-area').serialize();
+        
+        // error handling
+        if (tweetData.length < 6) {
+            $('#error-messages').text('Text area blank!');
+            $('#error-messages').show();
+            return;
+        } else if (tweetData.length > 147) {
+            $('#error-messages').text('Tweet must be 140 chars or less!');
+            $('#error-messages').show();
+            return;
+        } else {
+            // ajax submit
+            $.ajax('/tweets', { 
+                method: 'POST',
+                data: tweetData
+            }).then(function() {
+                // clear the form and load current tweet
+                $('#error-messages').hide();
+                $('#tweet-text-area').val('');
+                $('#text-counter').text(140);
+                
+                // remove all tweets and load list with newest tweet
+                // (could it be possible to just add on the last one without reloading all)
+                $('article').detach();
+                loadTweets();
+            });
+        }
+    });
+
+    $('#compose-button').on('click', function() {
+        $('#new-tweet-id').slideToggle(500);
+        $('#tweet-text-area').focus();
+    })
+
 });
